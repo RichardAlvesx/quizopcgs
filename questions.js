84f660c1beaa6585c2cfee4191cfbaf03d383882
RichardAlvesx <134708827+RichardAlvesx@users.noreply.github.com>
2023-08-29 21:37:42 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "Em qual cidade paranaense está localizado o contrato BOTICÁRIO-PR?",
+    answers: [
+      { option: "Curitiba - PR", correct: false },
+      { option: "Campina Grande do Sul - PR", correct: true },
+      { option: "Cidade Industrial - PR", correct: false },
+    ],
+  },
+  {
+    question: "Quais são os dois sites e-commerce ligados ao CD?",
+    answers: [
+      { option: "Beleza na Web e Boticário", correct: true },
+      { option: "Eudora e Quem Disse Berenice?", correct: false },
+      { option: "Boticário e Eudora", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o nosso nível atual em auditoria SCA?",
+    answers: [
+      { option: "Showcase", correct: false },
+      { option: "Desenvolvedor", correct: false },
+      { option: "Avançado", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o nosso fluxo operacional?",
+    answers: [
+      { option: "Recebimento → Armazenagem → Ressuprimento → Faturamento → Expedição", correct: false },
+      { option: "Armazenagem → Ressuprimento → Picking → Expedição", correct: false },
+      { option: "Recebimento → Armazenagem → Ressuprimento → Picking → Faturamento → Expedição", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o nosso target NET e GROSS de Inventário?",
+    answers: [
+      { option: "NET: 99,95% | GROSS: 99,80%", correct: false },
+      { option: "NET: 99,85% | GROSS: 99,95%", correct: false },
+      { option: "NET: 99,95% | GROSS: 99,85%", correct: true },
+    ],
+  },
+];
---
+++ script.js
@@ -0,0 +1,86 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const textRestart = document.querySelector(".restart span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const contentRestart = document.querySelector(".restart");
+const btnFinish = document.querySelector(".finish button");
+const btnRestart = document.querySelector(".restart button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentRestart.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+btnFinish.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  window.location.href = "digitarnome.html";
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  } else {
+    alert('Resposta errada!');
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  if(questionsCorrect == questions.length)
+  {
+    textFinish.innerHTML = `<center>Muito bem..<br>Você acertou todas as questões!<br>Clique abaixo para gerar seu certificado:`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+  }else{
+    textRestart.innerHTML = `<center>Poxa.. Você não acertou todas as questões :(<br>Você acertou 0${questionsCorrect} de 0${questions.length} questões!<br>Tente novamente...`;
+    content.style.display = "none";
+    contentRestart.style.display = "flex";   
+  }
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `Questão: 0${currentIndex + 1} de 0${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
