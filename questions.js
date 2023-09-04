f869661d5be66b3623b409181ae604fe51658f6b
RichardAlvesx <134708827+RichardAlvesx@users.noreply.github.com>
2023-09-04 19:38:42 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "Em qual cidade paranaense está localizado o contrato BOTICÁRIO-PR?",
+    answers: [
+      { option: "Curitiba - PR", correct: false },
+      { option: "Campina Grande do Sul - PR", correct: true },
+      { option: "Cidade Industrial - PR", correct: false },
+    ],
+  },
+  {
+    question: "Quais são os sites e-commerce ligados ao CD?",
+    answers: [
+      { option: "Beleza na Web, O Boticário, Eudora e Quem disse Berenice?", correct: true },
+      { option: "Jequiti, Natura, O Boticário e Eudora", correct: false },
+      { option: "Beleza na Web, O Boticário e Quem disse Berenice?", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o nosso nível atual em auditoria SCA?",
+    answers: [
+      { option: "Showcase", correct: false },
+      { option: "Desenvolvimento", correct: false },
+      { option: "Avançado", correct: true },
+    ],
+  },
+  {
+    question: "Qual a cidade e estado de origem da marca O Boticário?",
+    answers: [
+      { option: "Curitiba - PR", correct: true },
+      { option: "Registro - SP", correct: false },
+      { option: "Copacabana - RJ", correct: false },
+    ],
+  },
+  {
+    question: "Qual o ano de origem da marca O Boticário?",
+    answers: [
+      { option: "1877", correct: false },
+      { option: "1987", correct: false },
+      { option: "1977", correct: true },
+    ],
+  },
+];
